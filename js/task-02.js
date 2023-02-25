fd9203aa3dac37f9858e89311a30c33ff7b66b29
onsnov <114084570+onsnov@users.noreply.github.com>
2023-02-25 10:17:38 +0200

    додає завдання 2

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,15 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsList = document.querySelector("#ingredients");
+ingredients.forEach(ingredient => {
+  const li = document.createElement("li");
+  li.textContent = ingredient;
+  li.classList.add('item');
+ingredientsList.append(li);
+ })
+
+
+
+
