f59cdb02ff10eb549288e609bf6723fbab0ff691
onsnov <114084570+onsnov@users.noreply.github.com>
2023-03-07 12:27:51 +0200

    додає 6 завдання

---
+++ js/task-06.js
@@ -0,0 +1,17 @@
+const validation = document.querySelector("#validation-input");
+const textDatasetLength = Number(validation.dataset.length);
+validation.addEventListener("blur", eventHandler);
+
+function eventHandler(event) {
+    if (event.currentTarget.value.length === textDatasetLength) {
+        validation.classList.remove("invalid");
+        validation.classList.add("valid");
+    } else {
+         validation.classList.remove("valid");
+         validation.classList.add("invalid");   
+    }
+           
+};
+   
+
+
