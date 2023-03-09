540566bedf5da5018b36147851cfda9c392d25db
onsnov <114084570+onsnov@users.noreply.github.com>
2023-03-09 14:47:41 +0200

    додає 8 завдання

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
+const form = document.querySelector(".login-form");
+
+form.addEventListener("submit", handlerForm);
+function handlerForm(event) {
+
+    event.preventDefault();
+    const {
+      elements: { email, password },
+    } = event.currentTarget;
+
+  if (email.value === "" || password.value==="" ) {
+    return alert("всі поля повинні бути заповнені");
+  } else
+    console.log(`email: ${email
+        .value}, Password: ${password.value}`);
+    event.currentTarget.reset();
+}
+
+
+
+
+
 
