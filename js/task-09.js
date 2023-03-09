26d3b1b50cd1b611bbeb407955882468550d2ec6
onsnov <114084570+onsnov@users.noreply.github.com>
2023-03-09 16:55:57 +0200

    додає 9 завдання

--- js/task-09.js
+++ js/task-09.js
@@ -1,5 +1,18 @@
+const changeColor = document.querySelector("button.change-color");
+const bodyColor = document.querySelector('body');
+
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215)
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+changeColor.addEventListener("click", changeBgColor);
+
+function changeBgColor() { 
+bodyColor.style.backgroundColor = `${getRandomHexColor()}`;
+}
+console.log(body.style.backgroundColor);
+
+
