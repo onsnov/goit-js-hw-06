62788601f68df3bf2bffce733fe1127613af11ae
onsnov <114084570+onsnov@users.noreply.github.com>
2023-03-10 16:01:41 +0200

    змінює вивід в "об'єкт" в завданні 8

--- js/task-08.js
+++ js/task-08.js
@@ -3,16 +3,16 @@ const form = document.querySelector(".login-form");
 form.addEventListener("submit", handlerForm);
 function handlerForm(event) {
 
-    event.preventDefault();
-    const {
-      elements: { email, password },
-    } = event.currentTarget;
+  event.preventDefault();
+  const {
+    elements: { email, password },
+  } = event.currentTarget;
 
-  if (email.value === "" || password.value==="" ) {
+  if (email.value === "" || password.value === "") {
     return alert("всі поля повинні бути заповнені");
   } else
-    console.log(`email: ${email
-        .value}, Password: ${password.value}`);
+    console.log(`{email: ${email
+      .value}, Password: ${password.value}}`);
     event.currentTarget.reset();
 }
 
