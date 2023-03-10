4ba81ae4f189677cc22d79c74989c278c9187dd4
onsnov <114084570+onsnov@users.noreply.github.com>
2023-03-10 13:50:17 +0200

    змінює завдання 1 враховуючи навігацію по дом

--- js/task-01.js
+++ js/task-01.js
@@ -1,13 +1,15 @@
 
+// const categories = document.querySelector("#categories").querySelectorAll("li.item");
+
 const categories = document.querySelector("#categories").querySelectorAll("li.item");
-// const categoriesItem = categories
+ const categoriesItem = categories
 
  console.log(`Number of categories: ${categoriesItem.length}`);
 
 
  categoriesItem.forEach(item => {
-        const categoriesName = item.querySelector("h2").textContent;
-        const categoriesElements = item.querySelectorAll("li").length;
+        const categoriesName = item.firstElementChild.textContent;
+        const categoriesElements = item.lastElementChild.length;
         console.log(`Category: ${categoriesName}`);
         console.log(`Elements: ${categoriesElements}`);
     });
