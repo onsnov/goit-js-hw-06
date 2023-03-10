6ba955992eec22de144fec0ffc9ef879f55f59de
onsnov <114084570+onsnov@users.noreply.github.com>
2023-03-10 14:48:34 +0200

    зміню слухач на input в 7 завданні

--- js/task-07.js
+++ js/task-07.js
@@ -1,7 +1,7 @@
 const inputField = document.querySelector("#font-size-control");
 const text = document.querySelector("#text");
 
-inputField.addEventListener("change", inputHendlerChangeFontSize);
+inputField.addEventListener("input", inputHendlerChangeFontSize);
 function inputHendlerChangeFontSize(event) {
     text.style.fontSize = event.currentTarget.value + 'px'
 };
